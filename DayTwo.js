efdfa152c997c0904a52c32e3df5afdfc66b100c
Lazar Kiridjievski <l.kiridzievski@gmail.com>
2023-12-02 17:32:31 +0100

    second day

--- DayTwo.js
+++ DayTwo.js
@@ -5,7 +5,7 @@ const data = fs.readFileSync('input.txt', 'utf-8').split('\r\n');
 // First Star
 
 const games = [];
-let sumOfValidGames = 0;
+let sum = 0;
 
 for (let game of data) {
    let gameData = game.split('Game')[1];
@@ -40,15 +40,47 @@ for (let i = 0; i < games.length; i++) {
    }
 }
 
-
 // Second Star
 
-// const { red, green, blue } = cubes.reduce((sum, data) => {
-//    const color = Object.keys(data)[0];
-//    const value = data[color];
-//    sum[color] = (sum[color] || 0) + value;
-//    return sum;
-// }, {});
+for (let game of data) {
+   let gameData = game.split('Game')[1];
+   gameData = gameData.replaceAll(';', ',');
+   gameData = gameData.split(':');
+   const key = gameData[0].trim();
+   const values = gameData[1].split(',');
 
+   const valuesArray = [];
 
-console.log('sumOfValidGames', sumOfValidGames);
+   for (let i = 0; i < values.length; i++) {
+      let value = values[i].trim();
+      value = value.split(' ');
+      valuesArray.push({ [value[1]]: +value[0] });
+   }
+   games.push({ [key]: valuesArray });
+}
+
+for (let i = 0; i < games.length; i++) {
+   const gameId = Object.keys(games[i])[0];
+   const cubes = Object.values(games[i])[0];
+
+   let red = 0;
+   let green = 0;
+   let blue = 0;
+
+   for (const cube of cubes) {
+      if (cube.red && cube.red > red) {
+         red = cube.red;
+      }
+      if (cube.green && cube.green > green) {
+         green = cube.green;
+      }
+      if (cube.blue && cube.blue > blue) {
+         blue = cube.blue;
+      }
+   }
+
+   const power = red * green * blue;
+   sum += power;
+}
+
+console.log('sum', sum);
