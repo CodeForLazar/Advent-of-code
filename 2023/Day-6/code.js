6cf9608882102ba8bd8b44488c6e23bad1311c7c
Lazar Kiridjievski <l.kiridzievski@gmail.com>
2023-12-07 20:55:51 +0100

    day 6

---
+++ 2023/Day-6/code.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8').split('\r\n');
+
+const game = data.map((info) => info.split(':')[1].trim().replace(/^\s+|\s+$|\s+(?=\s)/g, "").split(' '));
+
+exports.fistStar = () => {
+   let sum = 1;
+   let waysToWin = [0, 0, 0, 0];
+   const time = game[0];
+   const distance = game[1];
+   for (let i = 0; i < time.length; i++) {
+      const timeVal = time[i];
+      const DistanceVal = distance[i];
+      let buttonHold = timeVal;
+      for (let sec = 0; sec <= timeVal; sec++) {
+         if (sec * buttonHold > DistanceVal) {
+            waysToWin[i]++;
+         }
+         buttonHold = buttonHold - 1;
+      }
+   }
+
+   sum = waysToWin.reduce((acc, num) => acc * num)
+   console.log('DaySix-PartOne', sum);
+};
+
+exports.secondStar = () => {
+   let sum = 0;
+
+   console.log('DaySix-PartOne', sum);
+};
--- 2023/index.js
+++ 2023/index.js
@@ -1,12 +1,16 @@
 const dayOne = require('./Day-1/code');
 const dayTwo = require('./Day-2/code');
 const dayFour = require('./Day-4/code');
+const daySix = require('./Day-6/code');
 
-dayOne.fistStar();
-dayOne.secondStar();
+// dayOne.fistStar();
+// dayOne.secondStar();
 
-dayTwo.fistStar();
-dayTwo.secondStar();
+// dayTwo.fistStar();
+// dayTwo.secondStar();
 
-dayFour.fistStar();
-dayFour.secondStar();
+// dayFour.fistStar();
+// dayFour.secondStar();
+
+daySix.fistStar();
+daySix.secondStar();
