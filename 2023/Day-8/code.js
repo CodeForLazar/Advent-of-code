740f23e55b62905db7e7f5b2a44a15d1633e7abd
Lazar Kiridjievski <l.kiridzievski@gmail.com>
2023-12-25 14:43:23 +0100

    day 8 part 2

--- 2023/Day-8/code.js
+++ 2023/Day-8/code.js
@@ -62,11 +62,6 @@ const rows = game.reduce((acc, el, idx) => {
 
 exports.secondStar = () => {
    let sum = 0;
-   let sum2 = 0;
-   let sum3 = 0;
-   let sum4 = 0;
-   let sum5 = 0;
-   let sum6 = 0;
    let done = false;
    const mapLR = {
       L: 1,
@@ -86,11 +81,6 @@ exports.secondStar = () => {
    let target5 = game.find((_, idx) => idx === row5);
    let target6 = game.find((_, idx) => idx === row6);
    let j = 0;
-   let j2 = 0;
-   let j3 = 0;
-   let j4 = 0;
-   let j5 = 0;
-   let j6 = 0;
    let nextTarget;
    let nextTarget2;
    let nextTarget3;
@@ -98,98 +88,81 @@ exports.secondStar = () => {
    let nextTarget5;
    let nextTarget6;
    let seq;
-   let seq2;
-   let seq3;
-   let seq4;
-   let seq5;
-   let seq6;
 
    while (!done) {
-      if (row >= game.length) {
-         row = 0;
-      }
-      if (row2 >= game.length) {
-         row2 = 0;
-      }
-      if (row3 >= game.length) {
-         row3 = 0;
-      }
-      if (row4 >= game.length) {
-         row4 = 0;
-      }
-      if (row5 >= game.length) {
-         row5 = 0;
-      }
-      if (row6 >= game.length) {
-         row6 = 0;
-      }
-
       if (j >= sequence.length) {
          j = 0;
       }
-      if (j2 >= sequence.length) {
-         j2 = 0;
-      }
-      if (j3 >= sequence.length) {
-         j3 = 0;
-      }
-      if (j4 >= sequence.length) {
-         j4 = 0;
-      }
-      if (j5 >= sequence.length) {
-         j5 = 0;
-      }
-      if (j6 >= sequence.length) {
-         j6 = 0;
-      }
 
       seq = mapLR[sequence[j]];
-      seq2 = mapLR[sequence[j2]];
-      seq3 = mapLR[sequence[j3]];
-      seq4 = mapLR[sequence[j4]];
-      seq5 = mapLR[sequence[j5]];
-      seq6 = mapLR[sequence[j6]];
       nextTarget = target[seq];
-      nextTarget2 = target2[seq2];
-      nextTarget3 = target3[seq3];
-      nextTarget4 = target4[seq4];
-      nextTarget5 = target5[seq5];
-      nextTarget6 = target6[seq6];
+      nextTarget2 = target2[seq];
+      nextTarget3 = target3[seq];
+      nextTarget4 = target4[seq];
+      nextTarget5 = target5[seq];
+      nextTarget6 = target6[seq];
 
-      target = game.find(el => {
-
-      })
-      if (game[row][0] === nextTarget) {
+      while (game[row][0] !== nextTarget) {
          target = game[row];
-         j++;
-         
-      if (game[row2][0] === nextTarget2) {
-         target2 = game[row2];
-         j2++;
-         
-      }
-      if (game[row3][0] === nextTarget3) {
-         target3 = game[row3];
-         j3++;
-         
-      }
-      if (game[row4][0] === nextTarget4) {
-         target4 = game[row4];
-         j4++;
-         
-      }
-      if (game[row5][0] === nextTarget5) {
-         target5 = game[row5];
-         j5++;
-         
-      }
-      if (game[row6][0] === nextTarget6) {
-         target6 = game[row6];
-         j6++;
-         
+         if (game[row][0] === nextTarget) {
+            break;
+         }
+         row++;
+         if (row >= game.length) {
+            row = 0;
+         }
       }
 
-      sum++
+      while (game[row2][0] !== nextTarget2) {
+         target2 = game[row2];
+         if (game[row2][0] === nextTarget2) {
+            break;
+         }
+         row2++;
+         if (row2 >= game.length) {
+            row2 = 0;
+         }
+      }
+      while (game[row3][0] !== nextTarget3) {
+         target3 = game[row3];
+         if (game[row3][0] === nextTarget3) {
+            break;
+         }
+         row3++;
+         if (row3 >= game.length) {
+            row3 = 0;
+         }
+      }
+      while (game[row4][0] !== nextTarget4) {
+         target4 = game[row4];
+         if (game[row4][0] === nextTarget4) {
+            break;
+         }
+         row4++;
+         if (row4 >= game.length) {
+            row4 = 0;
+         }
+      }
+      while (game[row5][0] !== nextTarget5) {
+         target5 = game[row5];
+         if (game[row5][0] === nextTarget5) {
+            break;
+         }
+         row5++;
+         if (row5 >= game.length) {
+            row5 = 0;
+         }
+      }
+      while (game[row6][0] !== nextTarget6) {
+         target6 = game[row6];
+         if (game[row6][0] === nextTarget6) {
+            break;
+         }
+         row6++;
+         if (row6 >= game.length) {
+            row6 = 0;
+         }
+      }
 
       if (
          target[0][2] === 'Z' &&
@@ -202,18 +175,10 @@ exports.secondStar = () => {
          done = true;
       }
 
-      row++;
-      row2++;
-      row3++;
-      row4++;
-      row5++;
-      row6++;
+      j++;
+
+      sum++;
    }
 
    console.log('DaySix-PartTwo', sum);
-   console.log('DaySix-PartTwo', sum2);
-   console.log('DaySix-PartTwo', sum3);
-   console.log('DaySix-PartTwo', sum4);
-   console.log('DaySix-PartTwo', sum5);
-   console.log('DaySix-PartTwo', sum6);
 };
