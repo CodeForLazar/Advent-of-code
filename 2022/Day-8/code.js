823f5f34bea350f69f95934891fb8542eea4be1c
Lazar Kiridjievski <l.kiridzievski@gmail.com>
2024-01-11 22:58:28 +0100

    day 8 part two

--- 2022/Day-8/code.js
+++ 2022/Day-8/code.js
@@ -60,7 +60,64 @@ exports.firstStar = () => {
 };
 
 exports.secondStar = () => {
-   let sum = 0;
+   let sum = [];
+
+   for (let i = 0; i < formatData.length; i++) {
+      const row = formatData[i];
+
+      for (let j = 0; j < row.length; j++) {
+         let top = 0;
+         let bottom = 0;
+         let left = 0;
+         let right = 0;
+         const target = row[j];
+
+         if (!formatData[i + 1] || !formatData[i - 1] || !row[j + 1] || !row[j - 1]) {
+            continue;
+         }
+
+         for (let d = i + 1; d < formatData.length; d++) {
+            const testBottom = formatData[d][j];
+            bottom += 1;
+            if (testBottom >= target) {
+               break;
+            }
+         }
+
+         for (let t = i - 1; t >= 0; t--) {
+            const testTop = formatData[t][j];
+            top += 1;
+            if (testTop >= target) {
+               break;
+            }
+         }
+
+         for (let r = j + 1; r < row.length; r++) {
+            const testRight = row[r];
+            right += 1;
+            if (target <= testRight) {
+               break;
+            }
+         }
+
+         for (let l = j - 1; l >= 0; l--) {
+            const testLeft = row[l];
+            left += 1;
+            if (target <= testLeft) {
+               break;
+            }
+         }
+
+         sum.push(left * right * bottom * top);
+      }
+   }
+
+   sum = sum.reduce((acc, num) => {
+      if (acc < num) {
+         acc = num;
+      }
+      return acc;
+   }, 0);
 
    console.log('DayEight-PartTwo', sum);
 };
