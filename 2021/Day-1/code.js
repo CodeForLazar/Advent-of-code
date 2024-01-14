6fe534001cd4ffbf7b1e511760cac8b33e7777c5
Lazar Kiridjievski <l.kiridzievski@gmail.com>
2024-01-14 20:09:48 +0100

    Day 1 part two

--- 2021/Day-1/code.js
+++ 2021/Day-1/code.js
@@ -21,6 +21,28 @@ exports.firstStar = () => {
 
 exports.secondStar = () => {
    let sum = 0;
+   let threeSums = [];
+
+   for (let i = 0; i < data.length; i++) {
+      const num = +data[i];
+      const num2 = +data[i + 1];
+      const num3 = +data[i + 2];
+
+      if (!num3) break;
+
+      threeSums.push(num + num2 + num3);
+   }
+
+   let startNum = threeSums[0];
+
+   for (let i = 0; i < threeSums.length; i++) {
+      const num = +threeSums[i];
+
+      if (num > startNum) {
+         sum++;
+      }
+      startNum = num;
+   }
 
    console.log('DayOne-PartTwo', sum);
 };
