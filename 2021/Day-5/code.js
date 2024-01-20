e84517ab10dd6defc0b991e898fbed9f7d242c0c
Lazar Kiridjievski <l.kiridzievski@gmail.com>
2024-01-20 15:24:20 +0100

    Day5 part two

--- 2021/Day-5/code.js
+++ 2021/Day-5/code.js
@@ -73,7 +73,79 @@ exports.firstStar = () => {
 };
 
 exports.secondStar = () => {
-   let sum = [];
+   let sum = 0;
+   const map = Array.from({ length: rowLength + 1 }, () => Array(colLength + 1).fill(0));
+
+   for (let i = 0; i < data.length; i++) {
+      const row = data[i];
+      const x1 = row[0];
+      const y1 = row[1];
+      const x2 = row[2];
+      const y2 = row[3];
+
+      if (x1 === x2) {
+         if (y1 < y2) {
+            for (let j = y1; j <= y2; j++) {
+               map[j][x1] = map[j][x1] + 1;
+            }
+         } else {
+            for (let j = y1; j >= y2; j--) {
+               map[j][x1] = map[j][x1] + 1;
+            }
+         }
+      }
+
+      if (y1 === y2) {
+         if (x1 < x2) {
+            for (let j = x1; j <= x2; j++) {
+               map[y1][j] = map[y1][j] + 1;
+            }
+         } else {
+            for (let j = x1; j >= x2; j--) {
+               map[y1][j] = map[y1][j] + 1;
+            }
+         }
+      }
+
+      if (Math.abs(y1 - y2) === Math.abs(x1 - x2)) {
+         if (y2 > y1) {
+            let col = x1;
+            if (x1 < x2) {
+               for (let j = y1; j <= y2; j++) {
+                  map[j][col] = map[j][col] + 1;
+                  col++;
+               }
+            } else {
+               for (let j = y1; j <= y2; j++) {
+                  map[j][col] = map[j][col] + 1;
+                  col--;
+               }
+            }
+         } else {
+            if (x1 < x2) {
+               let col = x2;
+               for (let j = y2; j <= y1; j++) {
+                  map[j][col] = map[j][col] + 1;
+                  col--;
+               }
+            } else {
+               let col = x2;
+               for (let j = y2; j <= y1; j++) {
+                  map[j][col] = map[j][col] + 1;
+                  col++;
+               }
+            }
+         }
+      }
+   }
+
+   for (const row of map) {
+      for (const col of row) {
+         if (col >= 2) {
+            sum += 1;
+         }
+      }
+   }
 
    console.log('DayFive-PartOne', sum);
 };
