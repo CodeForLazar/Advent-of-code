7738aa560ae3156ec97e3fe4538778a51262dd78
Lazar Kiridjievski <l.kiridzievski@gmail.com>
2024-04-03 22:42:32 +0200

    day 2 part two

--- 2019/Day-1/code.js
+++ 2019/Day-1/code.js
@@ -5,21 +5,25 @@ const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf-8').split('
 
 exports.firstStar = () => {
    let sum = [];
-
    for (let i = 0; i < data.length; i++) {
       const mass = +data[i];
       const gas = Math.floor(mass / 3) - 2;
       sum.push(gas);
    }
-
    sum = sum.reduce((acc, num) => acc + num, 0);
-   
    console.log('DayOne-PartOne', sum);
 };
 
 exports.secondStar = () => {
-   let sum = 0;
+   let sum = [];
+   for (let i = 0; i < data.length; i++) {
+      let mass = +data[i];
 
-
+      while (Math.floor(mass / 3) - 2 > 0) {
+         mass = Math.floor(mass / 3) - 2;
+         sum.push(mass);
+      }
+   }
+   sum = sum.reduce((acc, num) => acc + num, 0);
    console.log('DayOne-PartTwo', sum);
 };
